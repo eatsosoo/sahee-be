e69e6ba5c04fa93cd77bfb74a8daf222eac1fac4
PhongTD <eatsoosoo@gmail.com>
2024-06-23 20:43:56 +0700

    fix register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:8',
+            'password' => 'required|string|min:6',
         ]);
 
         $user = new User();
